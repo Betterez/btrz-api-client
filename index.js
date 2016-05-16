631eb3888efb93705a35ac302ccd6f4b3e479d4d
hgarcia <hernan@dynamicprogrammer.com>
2016-05-16 16:47:38 -0400

    [HG] Adding request.

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 "use strict";
-let Swagger = require('swagger-client');
-let http = require("./http");
-
 class BtrzApi {
-
-	constructor(domain, apiKey, token, logger) {
-		this.inventory = new Swagger({url: `${domain}inventory/api-docs`, usePromise: true })
-			.then((client) => {
-        client.clientAuthorizations.add("apiKey", new Swagger.ApiKeyAuthorization("X-API-KEY", apiKey, "header"));
-        client.clientAuthorizations.add("token", new Swagger.ApiKeyAuthorization("authorization", `Bearer ${token}`, "header"));
-        return client;
-    	})
-    	.catch((err) => {
-        console.log("in the constructor: ", err);
-    	});
-	}
-
-	help() {
-		return Promise.resolve("inventory");
-	}
 }
 
 exports.BtrzApi = BtrzApi;
