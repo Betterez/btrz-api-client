bd5dada373c77e33328721f77f98be81e7f93219
Martin Silva <msmartinsilva@gmail.com>
2017-04-25 10:01:21 -0300

    enabled

--- src/endpoints/inventory/insurances.js
+++ src/endpoints/inventory/insurances.js
@@ -1,21 +1,33 @@
+const url = require("url");
+
 function insurancesFactory({ client }) {
   
   function all({ token }) {
-    return client.get("/inventory/insurances", {
+    return client.get("/inventory/insurances/", {
       headers: { 'x-api-key': `${token}`}
     });
   }
 
   function create({ token, insurance }) {
-    return client.post("/inventory/insurances", {
+    return client.post("/inventory/insurances/", {
       headers: { 'x-api-key': `${token}`},
       data: insurance
     });
   }
 
+  function enabled({ token, insurance }) {
+    const one = url.resolve("/inventory/insurances/", insurance._id);
+    console.log(one);
+    return client.patch(one, {
+      headers: { 'x-api-key': `${token}`},
+      data: { enabled: insurance.enabled }
+    });
+  }
+
   return { 
     all,
-    create
+    create,
+    enabled
   };
 }
 
--- test/endpoints/inventory/insurances.test.js
+++ test/endpoints/inventory/insurances.test.js
@@ -5,8 +5,9 @@ describe('inventory/insurances', function() {
   const token = 'I owe you a token';
   
   before(function() {
-    axiosMock.onGet(`/inventory/insurances`).reply(200);
-    axiosMock.onPost(`/inventory/insurances`).reply(200);
+    axiosMock.onGet(`/inventory/insurances/`).reply(200);
+    axiosMock.onPost(`/inventory/insurances/`).reply(200);
+    axiosMock.onPatch(`/inventory/insurances/1`).reply(200);
   });
 
   it("should list insurances", function() {
@@ -14,7 +15,11 @@ describe('inventory/insurances', function() {
   });
 
   it("should create insurances", function() {
-    return client.inventory.insurances.create({ token, insurance: { cost: 1000, threshold: 1000 } })
+    return client.inventory.insurances.create({ token, insurance: { cost: 1000, threshold: 1000 } });
+  });
+
+  it("should enabled insurance", function() {
+    return client.inventory.insurances.enabled({ token, insurance: { _id: "1", enabled: true } });
   });
 
 });
