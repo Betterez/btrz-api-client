c5e87dbad8b52bd9fe8cbd89bc82d4f46b6df14c
Damian Mantuano <smarbos@gmail.com>
2018-09-27 14:09:26 -0300

    [DM][Amenities] Restored enviromental vars on integration tests

--- test-integration/endpoints/inventory/amenities.test.js
+++ test-integration/endpoints/inventory/amenities.test.js
@@ -1,10 +1,8 @@
 const { expect } = require("chai");
 const port = process.env.INVENTORY_API_PORT;
 const token = process.env.API_TOKEN;
-const jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6IjUxYWY3ZGM5MzFjNzI5Yjc1YzAwMDA4YSIsImVtYWlsIjoic3VzbWl0aGErYmV0dGVyQGJldHRlcmV6LmNvbSIsImlhdCI6MTUzNjg2NTAyNSwiZXhwIjoxNTM3MDM3ODI1LCJhdWQiOiJiZXR0ZXJlei1hcHAiLCJpc3MiOiJidHJ6LWFwaS1hY2NvdW50cyIsInN1YiI6ImFjY291bnRfdXNlcl9zaWduX2luIn0.iMEzVOhk_neLcwSQJV90siFjAYk8IQCTkNfchOdy26sW5QxuLo7Rc1VUMoVKbCBElROBnquU6vQMV_2kvePJkQ";
-const amenityId = "5b806c89be89cfe13d3651d3";
-// const jwtToken = process.env.JWT_TOKEN;
-// const amenityId = process.env.AMENITY_ID;
+const jwtToken = process.env.JWT_TOKEN;
+const amenityId = process.env.AMENITY_ID;
 
 const api = require("./../../../src/client").createApiClient({
   baseURL: `http://localhost:${port}`,
