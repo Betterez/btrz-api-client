914c197e0ca7ff61a0496f27d9df33fa7a322a81
Damian Mantuano <smarbos@gmail.com>
2018-09-27 14:42:37 -0300

    [DM] Removed .only from tests

--- test/endpoints/inventory/amenities.test.js
+++ test/endpoints/inventory/amenities.test.js
@@ -1,7 +1,7 @@
 const { axiosMock, expectRequest } = require("./../../test-helpers");
 const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
 
-describe.only('inventory/amenities', () => {
+describe('inventory/amenities', () => {
   const token = 'I owe you a token',
     jwtToken = 'I owe you a JWT token';
 
--- test/endpoints/inventory/amenity-groups.test.js
+++ test/endpoints/inventory/amenity-groups.test.js
@@ -1,7 +1,7 @@
 const { axiosMock, expectRequest } = require("./../../test-helpers");
 const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
 
-describe.only('inventory/amenity-groups', () => {
+describe('inventory/amenity-groups', () => {
   const token = 'I owe you a token',
     jwtToken = 'I owe you a JWT token';
 
