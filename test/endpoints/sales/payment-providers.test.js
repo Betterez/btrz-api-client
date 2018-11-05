7b88828e90f5aaa226ef931bbc8382a61651736c
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2018-11-05 15:16:46 -0300

    #9503: Add test for endpoint payment providers.

--- test/endpoints/sales/payment-providers.test.js
+++ test/endpoints/sales/payment-providers.test.js
@@ -2,7 +2,8 @@ const { axiosMock, expectRequest } = require("./../../test-helpers");
 const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
 
 describe('sales/payment-providers', function() {
-  const token = 'I owe you a token';
+  const token = 'I owe you a token',
+    jwtToken = 'I owe you a JWT token';
   
   afterEach(function() {
     axiosMock.reset();
@@ -11,6 +12,11 @@ describe('sales/payment-providers', function() {
   it("should list payment providers", function() {
     axiosMock.onGet(`/payment-providers`).reply(expectRequest({ statusCode: 200, token }));
     return api.sales.paymentProviders.all({ token });
+  });
+
+  it("should list payment providers when request include jwtToken", function() {
+    axiosMock.onGet(`/payment-providers`).reply(expectRequest({ statusCode: 200, token, jwtToken}));
+    return api.sales.paymentProviders.all({ token, jwtToken });
   });
 
 });
