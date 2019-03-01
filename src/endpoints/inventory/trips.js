8b1cd4487f94fc8926dddfa02895b8e09ecc7b30
Pehuén <pehuen.rodriguez@nayra.coop>
2019-03-01 14:28:27 -0300

    #g9znu Added jwtToken to trips.all

--- src/endpoints/inventory/trips.js
+++ src/endpoints/inventory/trips.js
@@ -1,11 +1,11 @@
 const { authorizationHeaders } = require("./../endpoints_helpers");
 
 function tripsFactory({ client, internalAuthTokenProvider }) {
-  function all({ token, query = {} }) {
+  function all({ token, jwtToken, query = {} }) {
     return client({
       url: "/trips",
       params: query,
-      headers: authorizationHeaders({token, internalAuthTokenProvider})
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider})
     });
   }
 
