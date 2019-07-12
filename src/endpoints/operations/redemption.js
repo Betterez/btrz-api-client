3f8423db77571bbb736a6f6e31851a3303ca5aa2
lleveroni <lleveroni@eryx.co>
2019-07-11 21:53:21 -0300

    Add support for endpoint GET /operations/redemptions/validate/{passId}
    Bump version 1.2.0
    
    #zbzbg

--- src/endpoints/operations/redemption.js
+++ src/endpoints/operations/redemption.js
@@ -1,19 +1,27 @@
-const url = require("url");
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {authorizationHeaders} = require("./../endpoints_helpers");
 
-function redemptionFactory({ client, internalAuthTokenProvider }) {
-
-  function create({ token, jwtToken, redemption }) {
-    return client({ 
+function redemptionFactory({client, internalAuthTokenProvider}) {
+  function create({token, jwtToken, redemption}) {
+    return client({
       url: "/redemptions",
       method: "post",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider}),
       data: redemption
     });
   }
-  
+
+  function getValidate({token, jwtToken, passId, timezone}) {
+    return client({
+      url: `/redemptions/validate/${passId}`,
+      params: {timezone},
+      method: "get",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider})
+    });
+  }
+
   return {
-    create
+    create,
+    getValidate
   };
 }
 
--- test/endpoints/operations/redemption.test.js
+++ test/endpoints/operations/redemption.test.js
@@ -1,16 +1,15 @@
-const { axiosMock, expectRequest } = require("./../../test-helpers");
-const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
-const expect = require("chai").expect;
+const {axiosMock, expectRequest} = require("./../../test-helpers");
+const api = require("./../../../src/client").createApiClient({baseURL: "http://test.com"});
 
-describe('operations/redemptions', function() {
-  const token = 'I owe you a token';
-  const jwtToken = 'I owe you a JWT token'
-  
-  afterEach(function() {
+describe("operations/redemptions", () => {
+  const token = "I owe you a token";
+  const jwtToken = "I owe you a JWT token";
+
+  afterEach(() => {
     axiosMock.reset();
   });
 
-  it("should create a redemption", function() {
+  it("should create a redemption", () => {
     const redemption = {
       "itemCode": "PA-59e60182bc6ef25a2cfd6b6f",
       "options": {
@@ -21,13 +20,19 @@ describe('operations/redemptions', function() {
         "operationType": "received"
       },
       "redemptions": {
-          "items": [],
-          "scannedPurchases": [],
-          "externalPasses": []
+        "items": [],
+        "scannedPurchases": [],
+        "externalPasses": []
       }
     };
-    axiosMock.onPost(`/redemptions`).reply(expectRequest({ statusCode: 200, token, jwtToken }));
-    return api.operations.redemption.create({ jwtToken, token, redemption });
+    axiosMock.onPost("/redemptions").reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.operations.redemption.create({jwtToken, token, redemption});
   });
 
-}); 
+  it("should validate a redemption", () => {
+    const passId = "abc123";
+    const timezone = "America/Toronto";
+    axiosMock.onGet(`/redemptions/validate/${passId}`).reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.operations.redemption.getValidate({jwtToken, token, passId, timezone});
+  });
+});
