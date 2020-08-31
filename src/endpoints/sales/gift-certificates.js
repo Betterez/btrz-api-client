2330d41e6c0a2c0e03161c509e35ccddd08a26b7
hgarcia <hernan@dynamicprogrammer.com>
2020-08-31 16:19:26 -0400

    [HG] Adding support for the qs parameters as expected in the API.

--- src/endpoints/sales/gift-certificates.js
+++ src/endpoints/sales/gift-certificates.js
@@ -1,16 +1,19 @@
-const url = require("url");
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {
+  authorizationHeaders
+} = require("./../endpoints_helpers");
 
-function giftCertificatesFactory({ client, internalAuthTokenProvider }) {
-
-  function get({ token, GCNumber }) {
+function giftCertificatesFactory({
+  client, internalAuthTokenProvider
+}) {
+  function get({token, GCNumber, query = {}}) {
     return client({
       url: `/gift-certificates/${GCNumber}`,
+      params: query,
       headers: authorizationHeaders({token, internalAuthTokenProvider})
     });
   }
 
-  return { 
+  return {
     get
   };
 }
