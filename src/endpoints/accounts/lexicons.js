8864301d6e2e27def2f84b113d7d1522e7da587b
hgarcia <hernan@dynamicprogrammer.com>
2020-09-16 16:31:14 -0400

    [HG] adding method for createOrUpdateMany lexicons.

--- src/endpoints/accounts/lexicons.js
+++ src/endpoints/accounts/lexicons.js
@@ -1,42 +1,77 @@
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {
+  authorizationHeaders
+} = require("./../endpoints_helpers");
 
-function lexiconsFactory({ client, internalAuthTokenProvider }) {
-  
-  function all({ token, context, query = {} }) {
+function lexiconsFactory({
+  client, internalAuthTokenProvider
+}) {
+  function all({
+    token,
+    context,
+    query = {}
+  }) {
     const queryObj = Object.assign({}, query, {context});
 
-
     return client({
-      url: `lexicons/buscompany`,
+      url: "lexicons/buscompany",
       params: queryObj,
       headers: authorizationHeaders({token, internalAuthTokenProvider})
     });
   }
 
-  function create({ token, jwtToken, lexiconEntries }) {
+  function create({
+    token,
+    jwtToken,
+    lexiconEntries
+  }) {
     return client({
       url: "/lexicons",
       method: "post",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider}),
-      data: { entries: lexiconEntries }
+      data: {
+        entries: lexiconEntries
+      }
     });
   }
 
-  function updateMany({ token, jwtToken, updates }) {
+  function createOrUpdateMany({
+    token,
+    jwtToken,
+    entries
+  }) {
+    return client({
+      url: "/lexicons",
+      method: "put",
+      headers: authorizationHeaders({
+        token, jwtToken, internalAuthTokenProvider
+      }),
+      data: {
+        entries
+      }
+    });
+  }
+
+  function updateMany({
+    token,
+    jwtToken,
+    updates
+  }) {
     return client({
       url: "/lexicons",
       method: "patch",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider}),
-      data: { updates }
+      data: {
+        updates
+      }
     });
   }
 
-  return { 
+  return {
     all,
     create,
+    createOrUpdateMany,
     updateMany
   };
-
 }
 
 module.exports = lexiconsFactory;
--- test/endpoints/accounts/lexicons.test.js
+++ test/endpoints/accounts/lexicons.test.js
@@ -1,29 +1,48 @@
-const { axiosMock, expectRequest } = require("./../../test-helpers");
-const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
+const {
+  axiosMock, expectRequest
+} = require("./../../test-helpers");
+const api = require("./../../../src/client").createApiClient({
+  baseURL: "http://test.com"
+});
 
-describe('accounts/lexicons', function() {
-  const token = 'I owe you a token',
-    jwtToken = 'I owe you a JWT token';
-  
-  afterEach(function() {
+describe("accounts/lexicons", () => {
+  const token = "I owe you a token";
+  const jwtToken = "I owe you a JWT token";
+
+  afterEach(() => {
     axiosMock.restore();
-  })
-
-  it("should list countries", function() {
-    axiosMock.onGet(`/lexicons/buscompany`).reply((req) => {
-      if(req.params.context !== "some context" || req.params.someOtherParam !== 1) {
-        return [400];
-      } else {
-        return expectRequest({ statusCode: 200, token })(req);
-      }
-    });
-
-    return api.accounts.lexicons.all({ token, context: "some context", query: {someOtherParam: 1} });
   });
 
-  it("should create lexicon entries", function() {
-    axiosMock.onPost("/lexicons").reply(expectRequest({ statusCode: 200, token, jwtToken }));
-    return api.accounts.lexicons.create({ token, jwtToken, lexiconEntries: [{
+  it("should list lexicons", () => {
+    axiosMock.onGet("/lexicons/buscompany").reply((req) => {
+      if (req.params.context !== "some context" || req.params.someOtherParam !== 1) {
+        return [400];
+      }
+      return expectRequest({
+        statusCode: 200,
+        token
+      })(req);
+    });
+
+    return api.accounts.lexicons.all({
+      token,
+      context: "some context",
+      query: {
+        someOtherParam: 1
+      }
+    });
+  });
+
+  it("should create lexicon entries", () => {
+    axiosMock.onPost("/lexicons").reply(expectRequest({
+      statusCode: 200,
+      token,
+      jwtToken
+    }));
+    return api.accounts.lexicons.create({
+      token,
+      jwtToken,
+      lexiconEntries: [{
         accountId: "52f94137a8663b2704000009",
         name: "test-lexicon-entry-1",
         context: ["app", "websales", "vue"],
@@ -35,9 +54,37 @@ describe('accounts/lexicons', function() {
     });
   });
 
-  it("should update lexicon entries", function() {
-    axiosMock.onPatch("/lexicons").reply(expectRequest({ statusCode: 200, token, jwtToken }));
-    return api.accounts.lexicons.updateMany({ token, jwtToken, updates: [{
+  it("should create or update lexicon entries", () => {
+    axiosMock.onPut("/lexicons").reply(expectRequest({
+      statusCode: 200,
+      token,
+      jwtToken
+    }));
+    return api.accounts.lexicons.createOrUpdateMany({
+      token,
+      jwtToken,
+      entries: [{
+        accountId: "52f94137a8663b2704000009",
+        key: "test-lexicon-entry-1",
+        context: ["app", "websales", "vue"],
+        values: {
+          "en-us": "Updated english phrase",
+          "fr-fr": "Nouveau phrase en français"
+        }
+      }]
+    });
+  });
+
+  it("should update lexicon entries", () => {
+    axiosMock.onPatch("/lexicons").reply(expectRequest({
+      statusCode: 200,
+      token,
+      jwtToken
+    }));
+    return api.accounts.lexicons.updateMany({
+      token,
+      jwtToken,
+      updates: [{
         accountId: "52f94137a8663b2704000009",
         name: "test-lexicon-entry-1",
         context: ["app", "websales", "vue"],
@@ -48,5 +95,4 @@ describe('accounts/lexicons', function() {
       }]
     });
   });
-
 });
