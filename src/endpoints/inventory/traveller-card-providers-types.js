048d9d2309655b0eb26cd72a913ad4dc871c320b
jose.casanova <jose.casanova@nayra.coop>
2021-05-14 10:45:57 -0300

    typo fix

--- src/endpoints/inventory/traveller-card-providers-types.js
+++ src/endpoints/inventory/traveller-card-providers-types.js
@@ -2,7 +2,7 @@ const {authorizationHeaders} = require("./../endpoints_helpers");
 
 function travellerCardProvidersTypesFactory({client, internalAuthTokenProvider}) {
   function all({token, query = {}}) {
-    return client.get("/traveller-cards-providers/types", {
+    return client.get("/traveller-card-providers/types", {
       params: query,
       headers: authorizationHeaders({token, internalAuthTokenProvider})
     });
