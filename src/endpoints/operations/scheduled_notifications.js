e69b992074cf83c713b560ed461c328569bcae82
Fblind <fblind@gmail.com>
2021-06-10 15:54:58 -0300

    #u37fpq: Add scheduled notifications endpoints

--- src/endpoints/operations/scheduled_notifications.js
+++ src/endpoints/operations/scheduled_notifications.js
@@ -9,9 +9,48 @@ function scheduledNotificationsFactory({client, internalAuthTokenProvider}) {
     });
   }
 
+  function get({token, jwtToken, id}) {
+    return client({
+      url: `/scheduled-notifications/${id}`,
+      method: "get",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider})
+    });
+  }
+
+  function update({ token, jwtToken, id, data }) {
+    return client({
+      url: `/scheduled-notifications/${id}`,
+      method: "put",
+      params: data,
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider}),
+      data
+    });
+  }
+
+  function remove({ token, jwtToken, id }) {
+    return client({
+      url: `/scheduled-notifications/${id}`,
+      method: "delete",
+      headers: authorizationHeaders({ token, jwtToken, internalAuthTokenProvider })
+    });
+  }
+
+  function create({ token, jwtToken, query = {}, data }) {  
+    return client({
+      url: "/scheduled-notifications",
+      method: "post",
+      headers: authorizationHeaders({ token, jwtToken, internalAuthTokenProvider }),
+      params: query,
+      data
+    });
+  }
 
   return {
-    all
+    all,
+    get,
+    update,
+    remove,
+    create
   };
 }
 
