b28aad1c0336cb154b400a829883a1be3425bce3
José Sachs <jose@animus.com.ar>
2021-12-02 11:36:21 -0300

    #1t27jg8 Header params for tax-id endpoint

--- src/endpoints/invoices/tax-ids.js
+++ src/endpoints/invoices/tax-ids.js
@@ -1,11 +1,11 @@
 const {authorizationHeaders} = require("./../endpoints_helpers");
 
 function taxIdsFactory({client, internalAuthTokenProvider}) {
-  function all({token, jwtToken, query = {}}) {
+  function all({token, jwtToken, query = {}, headers}) {
     return client({
       url: "/tax-ids",
       method: "get",
-      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider}),
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
       params: query
     });
   }
