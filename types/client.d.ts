d911cb4fdc2a7eca2ea21800bc0d65e73e6c3df5
Hernan Garcia <hernan@dynamicprogrammer.com>
2022-03-31 13:11:15 -0400

    generated types from src

--- types/client.d.ts
+++ types/client.d.ts
@@ -2304,6 +2304,42 @@ export function createApiClient(options: {
         docs: {
             get: () => any;
         };
+        cardpointeTerminals: {
+            all: ({ token, jwtToken, headers }: {
+                token: any;
+                jwtToken: any;
+                headers: any;
+            }) => any;
+            remove: ({ token, jwtToken, merchantId, terminalId, headers }: {
+                token: any;
+                jwtToken: any;
+                merchantId: any;
+                terminalId: any;
+                headers: any;
+            }) => any;
+            readCard: {
+                get({ token, jwtToken, readCardResultId, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    readCardResultId: any;
+                    headers: any;
+                }): any;
+                create({ token, jwtToken, readCard, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    readCard: any;
+                    headers: any;
+                }): any;
+            };
+            ping: {
+                create({ token, jwtToken, ping, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    ping: any;
+                    headers: any;
+                }): any;
+            };
+        };
         paymentMethods: {
             all: ({ token, jwtToken, query, headers }: {
                 token: any;
--- types/initializedClient.d.ts
+++ types/initializedClient.d.ts
@@ -2258,6 +2258,42 @@ declare const _exports: {
         docs: {
             get: () => any;
         };
+        cardpointeTerminals: {
+            all: ({ token, jwtToken, headers }: {
+                token: any;
+                jwtToken: any;
+                headers: any;
+            }) => any;
+            remove: ({ token, jwtToken, merchantId, terminalId, headers }: {
+                token: any;
+                jwtToken: any;
+                merchantId: any;
+                terminalId: any;
+                headers: any;
+            }) => any;
+            readCard: {
+                get({ token, jwtToken, readCardResultId, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    readCardResultId: any;
+                    headers: any;
+                }): any;
+                create({ token, jwtToken, readCard, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    readCard: any;
+                    headers: any;
+                }): any;
+            };
+            ping: {
+                create({ token, jwtToken, ping, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    ping: any;
+                    headers: any;
+                }): any;
+            };
+        };
         paymentMethods: {
             all: ({ token, jwtToken, query, headers }: {
                 token: any;
