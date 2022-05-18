01ee276b192fec29998b95b43940db2580b95382
Hernan Garcia <hernan@dynamicprogrammer.com>
2022-05-18 17:20:05 -0400

    [HG] #2arkqu5

--- src/endpoints/accounts/network.js
+++ src/endpoints/accounts/network.js
@@ -3,19 +3,51 @@ const {
 } = require("../endpoints_helpers.js");
 
 function networkFactory({client, internalAuthTokenProvider}) {
-    const agencies = {
-        all({token, jwtToken, query = {}, headers}) {
-            return client({
-                url: "/network/agencies",
-                params: query,
-                headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
-            });
-        }
-    };
+  const agencies = {
+      all({token, jwtToken, query = {}, headers}) {
+          return client({
+              url: "/network/agencies",
+              params: query,
+              headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
+          });
+      },
+      get({token, query, headers, sellerId}) {
+        return client({
+          url: `/network/agencies/${sellerId}`,
+          params: query,
+          headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
+        });
+      },
+      update({jwtToken, token, sellerId, agency, headers, query}) {
+        return client({
+          url: `/network/agencies/${sellerId}`,
+          method: "put",
+          headers: authorizationHeaders({
+            token, jwtToken, internalAuthTokenProvider, headers
+          }),
+          data: {
+            agency
+          },
+          params: query
+        });
+      },
+      create({jwtToken, token, agency, headers}) {
+        return client({
+          url: "/network/agencies",
+          method: "post",
+          headers: authorizationHeaders({
+            token, jwtToken, internalAuthTokenProvider, headers
+          }),
+          data: {
+            agency
+          }
+        });
+      }
+  };
 
-    return {
-        agencies
-    };
+  return {
+    agencies
+  };
 }
 
 module.exports = networkFactory;
--- test/endpoints/accounts/network.test.js
+++ test/endpoints/accounts/network.test.js
@@ -29,5 +29,55 @@ describe("accounts/interline", () => {
     });
   });
 
+  it("should GET an agency by sellerID", () => {
+    axiosMock.onGet("/network/agencies/sellerId").reply(expectRequest({
+      statusCode: 200,
+      token,
+      jwtToken
+    }));
+    return api.accounts.network.agencies.get({
+      token,
+      sellerId: "sellerId",
+      jwtToken
+    }).then((httpResponse) => {
+      expect(httpResponse.status).eql(200);
+    });
+  });
+
+  it("should update an agency", () => {
+    const agency = {
+      name: "A"
+    };
+    const query = {
+      superUserId: "superUserId",
+      superUserHash: "superUserHash"
+    };
+    const sellerId = "A";
+    axiosMock.onPut(`/network/agencies/${sellerId}`)
+      .reply(expectRequest({
+        statusCode: 200,
+        token,
+        jwtToken
+      }));
+    return api.accounts.network.agencies.update({
+      token, jwtToken, agency, sellerId, query
+    });
+  });
+
+  it("should create an agency", () => {
+    const agency = {
+      name: "A"
+    };
+
+    axiosMock.onPost("/network/agencies")
+      .reply(expectRequest({
+        statusCode: 200,
+        token,
+        jwtToken
+      }));
+    return api.accounts.network.agencies.create({
+      token, jwtToken, agency
+    });
+  });
 
 });
