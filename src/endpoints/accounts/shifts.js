04784d546afb7574ab0a0cab775a00601203bacd
Damian Mantuano <smarbos@gmail.com>
2022-11-07 10:29:06 -0300

    [DM] #8rchhj Added Create and Update methods for shifts

--- src/endpoints/accounts/shifts.js
+++ src/endpoints/accounts/shifts.js
@@ -1,17 +1,44 @@
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {authorizationHeaders} = require("./../endpoints_helpers");
 
 function shiftsFactory({client, internalAuthTokenProvider}) {
-
   function get({token, userId, headers}) {
     return client.get(`/shift/user/${userId}`, {
       headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
     });
   }
 
-  return {
-    get
-  };
+  function create({jwtToken, token, shiftData, headers}) {
+    return client({
+      url: "/shifts",
+      method: "post",
+      headers: authorizationHeaders({
+        token, jwtToken, internalAuthTokenProvider, headers
+      }),
+      data: {
+        shiftData
+      }
+    });
+  }
 
+  function update({jwtToken, token, shiftId, operations, headers, query}) {
+    return client({
+      url: `/shifts/${shiftId}`,
+      method: "patch",
+      headers: authorizationHeaders({
+        token, jwtToken, internalAuthTokenProvider, headers
+      }),
+      data: {
+        operations
+      },
+      params: query
+    });
+  }
+
+  return {
+    get,
+    create,
+    update
+  };
 }
 
 module.exports = shiftsFactory;
