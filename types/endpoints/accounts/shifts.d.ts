90eeda711ab55aef601d38ea332ecfdece94cd5b
Hernan Garcia <hernan@dynamicprogrammer.com>
2023-02-03 14:31:26 -0500

    generated types from src

--- types/endpoints/accounts/shifts.d.ts
+++ types/endpoints/accounts/shifts.d.ts
@@ -28,4 +28,24 @@ declare function shiftsFactory({ client, internalAuthTokenProvider }: {
         headers: any;
         query: any;
     }) => any;
+    locationClosures: {
+        create({ jwtToken, token, locationClosure, headers }: {
+            jwtToken: any;
+            token: any;
+            locationClosure: any;
+            headers: any;
+        }): any;
+        all({ jwtToken, token, query, headers }: {
+            jwtToken: any;
+            token: any;
+            query: any;
+            headers: any;
+        }): any;
+        get({ token, jwtToken, locationClosureId, headers }: {
+            token: any;
+            jwtToken: any;
+            locationClosureId: any;
+            headers: any;
+        }): any;
+    };
 };
--- types/initializedClient.d.ts
+++ types/initializedClient.d.ts
@@ -1671,6 +1671,26 @@ declare const _exports: {
                 headers: any;
                 query: any;
             }) => any;
+            locationClosures: {
+                create({ jwtToken, token, locationClosure, headers }: {
+                    jwtToken: any;
+                    token: any;
+                    locationClosure: any;
+                    headers: any;
+                }): any;
+                all({ jwtToken, token, query, headers }: {
+                    jwtToken: any;
+                    token: any;
+                    query: any;
+                    headers: any;
+                }): any;
+                get({ token, jwtToken, locationClosureId, headers }: {
+                    token: any;
+                    jwtToken: any;
+                    locationClosureId: any;
+                    headers: any;
+                }): any;
+            };
         };
         shiftSettings: {
             get: ({ token, jwtToken, query, headers }: {
