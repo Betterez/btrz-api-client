415c57f441eaa3968623eb5e10babe4e47a17b6f
malenavero <malena@codigolibre.coop.ar>
2023-06-13 15:23:36 -0300

    [JC-MV] #865cfrztq agency support people lookups

--- src/endpoints/accounts/people-lookups.js
+++ src/endpoints/accounts/people-lookups.js
@@ -3,42 +3,51 @@ const {
 } = require("./../endpoints_helpers.js");
 
 function peopleLookupsFactory({client, internalAuthTokenProvider}) {
-  function all({token, jwtToken, query, headers}) {
+  function all({token, jwtToken, query, headers, providerId}) {
+    const query_ = providerId ? {...query, providerId} : query;
     return client({
       url: "/people-lookups",
-      params: query,
+      params: query_,
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
     });
   }
 
-  function getById({token, jwtToken, query, headers, personId}) {
+  function getById({token, jwtToken, query, headers, personId, providerId}) {
+    const query_ = providerId ? {...query, providerId} : query;
+
     return client({
       url: `/people-lookups/${personId}`,
-      params: query,
+      params: query_,
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
     });
   }
 
-  function update({jwtToken, token, personId, person, headers}) {
+  function update({jwtToken, token, personId, person, headers, providerId}) {
+    const query = providerId ? {providerId} : {};
+
     return client({
       url: `/people-lookups/${personId}`,
       method: "put",
       headers: authorizationHeaders({
         token, jwtToken, internalAuthTokenProvider, headers
       }),
+      params: query,
       data: {
         person
       }
     });
   }
 
-  function create({jwtToken, token, person, headers}) {
+  function create({jwtToken, token, person, headers, providerId}) {
+    const query = providerId ? {providerId} : {};
+
     return client({
       url: "/people-lookups",
       method: "post",
       headers: authorizationHeaders({
         token, jwtToken, internalAuthTokenProvider, headers
       }),
+      params: query,
       data: {
         person
       }
--- test/endpoints/accounts/people-lookups.test.js
+++ test/endpoints/accounts/people-lookups.test.js
@@ -1,9 +1,12 @@
 const {axiosMock, expectRequest} = require("./../../test-helpers.js");
 const api = require("./../../../src/client.js").createApiClient({baseURL: "http://test.com"});
+const expect = require("chai").expect;
+
 
 describe("accounts/people-lookups", () => {
   const token = "I owe you a token";
   const jwtToken = "secret";
+  const providerId = "5b9ab527c9a3ce576400043e";
 
   afterEach(() => {
     axiosMock.reset();
@@ -11,10 +14,18 @@ describe("accounts/people-lookups", () => {
 
   it("should get the people-lookups", () => {
     const query = {};
-
     axiosMock.onGet("/people-lookups", {params: query})
       .reply(expectRequest({statusCode: 200, token, jwtToken}));
     return api.accounts.peopleLookups.all({token, jwtToken, query});
+  });
+
+  it("should get the people-lookups with providerId", () => {
+    const query = {
+      providerId
+    };
+    axiosMock.onGet("/people-lookups", {params: query})
+      .reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.accounts.peopleLookups.all({token, jwtToken, query, providerId});
   });
 
   it("should get the a people-lookups by ID", () => {
@@ -27,12 +38,19 @@ describe("accounts/people-lookups", () => {
     return api.accounts.peopleLookups.getById({token, jwtToken, query, personId});
   });
 
-  it("should update a people lookup entry", () => {
-    const peopleLookup = {
-      doccumentType: "A",
-      documentNumber: "B"
-    };
+  it("should get the a people-lookups by ID with providerId", () => {
     const query = {
+      phone: "123456",
+      providerId
+    };
+    const personId = "personId";
+    axiosMock.onGet(`/people-lookups/${personId}`, {params: query})
+      .reply(expectRequest({statusCode: 200, token}));
+    return api.accounts.peopleLookups.getById({token, jwtToken, query, personId});
+  });
+
+  it("should update a people lookup entry", () => {
+    const person = {
       phone: "1234567"
     };
     const personId = "A";
@@ -42,13 +60,41 @@ describe("accounts/people-lookups", () => {
         token,
         jwtToken
       }));
+
     return api.accounts.peopleLookups.update({
-      token, jwtToken, peopleLookup, personId, query
-    });
+      jwtToken, token, personId, person
+    })
+      .then((res) => {
+        expect(res.config.params).to.eql({});
+        expect(res.config.data).to.eql(JSON.stringify({person}));
+        expect(res.status).to.equals(200);
+      });
+  });
+
+  it("should update a people lookup entry with providerId", () => {
+    const person = {
+      phone: "1234567"
+    };
+    const personId = "A";
+    axiosMock.onPut(`/people-lookups/${personId}`)
+      .reply(expectRequest({
+        statusCode: 200,
+        token,
+        jwtToken
+      }));
+
+    return api.accounts.peopleLookups.update({
+      jwtToken, token, personId, person, providerId
+    })
+      .then((res) => {
+        expect(res.config.params).to.eql({providerId});
+        expect(res.config.data).to.eql(JSON.stringify({person}));
+        expect(res.status).to.equals(200);
+      });
   });
 
   it("should create a people lookup entry", () => {
-    const peopleLookup = {
+    const person = {
       doccumentType: "A",
       documentNumber: "B"
     };
@@ -60,8 +106,35 @@ describe("accounts/people-lookups", () => {
         jwtToken
       }));
     return api.accounts.peopleLookups.create({
-      token, jwtToken, peopleLookup
-    });
+      token, jwtToken, person
+    })
+      .then((res) => {
+        expect(res.config.params).to.eql({});
+        expect(res.config.data).to.eql(JSON.stringify({person}));
+        expect(res.status).to.equals(200);
+      });
+  });
+
+  it("should create a people lookup entry with providerId", () => {
+    const person = {
+      doccumentType: "A",
+      documentNumber: "B"
+    };
+
+    axiosMock.onPost("/people-lookups")
+      .reply(expectRequest({
+        statusCode: 200,
+        token,
+        jwtToken
+      }));
+    return api.accounts.peopleLookups.create({
+      token, jwtToken, person, providerId
+    })
+      .then((res) => {
+        expect(res.config.params).to.eql({providerId});
+        expect(res.config.data).to.eql(JSON.stringify({person}));
+        expect(res.status).to.equals(200);
+      });
   });
 
   it("should delete the people lookup entry by id", () => {
