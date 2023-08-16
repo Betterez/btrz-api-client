28391520b59cea14221c0a4511b27133197f2a35
Hernan Garcia <hernan@dynamicprogrammer.com>
2023-08-16 13:58:08 -0400

    [HG] #860rhtzcn

--- src/endpoints/inventory/labels.js
+++ src/endpoints/inventory/labels.js
@@ -1,17 +1,17 @@
 const {authorizationHeaders} = require("../endpoints_helpers.js");
 
 function labelsFactory({client, internalAuthTokenProvider}) {
-  function all({token, query = {}, headers}) {
+  function all({token, jwtToken, query = {}, headers}) {
     return client.get("/labels", {
       params: query,
-      headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
     });
   }
 
-  function get({labelId, token, query = {}, headers}) {
+  function get({labelId, token, jwtToken, query = {}, headers}) {
     return client.get(`/labels/${labelId}`, {
       params: query,
-      headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
     });
   }
 
