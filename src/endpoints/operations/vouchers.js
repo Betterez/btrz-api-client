987356e50c045a9c6b8c7b9a61656df90ab8109a
Juan Manuel Fallo <juanmafallo@gmail.com>
2023-09-21 09:50:43 -0300

    [JMF] Rollback changes of #85zu09z9z

--- src/endpoints/operations/vouchers.js
+++ src/endpoints/operations/vouchers.js
@@ -6,13 +6,13 @@ function vouchersFactory({
   client, internalAuthTokenProvider
 }) {
   function create({
-    jwtToken, token, headers, query = {}, voucher = {}, user
+    jwtToken, token, headers, query = {}, voucher = {}
   }) {
     return client({
       url: "/vouchers",
       method: "POST",
       params: query,
-      data: {voucher, user},
+      data: voucher,
       headers: authorizationHeaders({
         jwtToken,
         token,
