342182c741fe7c8fd0ada6b9cca012a3dd24eb58
diegodindurra <diego@betterez.com>
2023-09-21 14:37:17 -0300

    #860pqmw8r. Seatmaps re-write. Rebuild the UI to generate without backbone.

--- src/endpoints/inventory/seatmaps.js
+++ src/endpoints/inventory/seatmaps.js
@@ -14,9 +14,50 @@ function seatmapsFactory({client, internalAuthTokenProvider}) {
     });
   }
 
+  function getById({seatmapId, token, jwtToken, query = {}, headers}) {
+    return client.get(`/seatmaps/${seatmapId}`, {
+      params: query,
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
+    });
+  }
+
+  function create({token, jwtToken, seatmap, headers}) {
+    return client({
+      url: "/seatmaps",
+      method: "post",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
+      data: {
+        seatmap
+      }
+    });
+  }
+
+  function update({token, jwtToken, seatmapId, seatmap, headers}) {
+    return client({
+      url: `/seatmaps/${seatmapId}`,
+      method: "put",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
+      data: {
+        seatmap
+      }
+    });
+  }
+
+  function remove({token, jwtToken, seatmapId, headers}) {
+    return client({
+      url: `/seatmaps/${seatmapId}`,
+      method: "delete",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
+    });
+  }
+
   return {
     all,
-    get
+    get,
+    getById,
+    create,
+    remove,
+    update
   };
 }
 
