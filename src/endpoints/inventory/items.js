1f30d726c3f4980ef9dff5e688666fea2ef07481
Hernan Garcia <hernan@dynamicprogrammer.com>
2024-04-05 15:47:51 -0400

    [HG] #86dt6hqd3

--- src/endpoints/inventory/items.js
+++ src/endpoints/inventory/items.js
@@ -1,19 +1,52 @@
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {
+  authorizationHeaders
+} = require("./../endpoints_helpers.js");
 
-function itemsFactory({ client, internalAuthTokenProvider }) {
-  
-  function all({ token, query = {}, headers }) {
+function itemsFactory({client, internalAuthTokenProvider}) {
+  function all({
+    token,
+    query = {},
+    jwtToken,
+    headers
+  }) {
     return client({
       url: "/items",
       params: query,
-      headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
     });
   }
 
-  return { 
-    all 
-  };
+  function get({itemId, token, jwtToken, query = {}, headers}) {
+    return client.get(`/items/${itemId}`, {
+      params: query,
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
+    });
+  }
 
+  function create({jwtToken, item, token, headers}) {
+    return client({
+      url: "/items",
+      method: "post",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
+      data: {item}
+    });
+  }
+
+  function update({jwtToken, token, itemId, item, headers}) {
+    return client({
+      url: `/items/${itemId}`,
+      method: "put",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
+      data: {item}
+    });
+  }
+
+  return {
+    all,
+    get,
+    create,
+    update
+  };
 }
 
 module.exports = itemsFactory;
--- test/endpoints/inventory/items.test.js
+++ test/endpoints/inventory/items.test.js
@@ -1,16 +1,60 @@
-const { axiosMock, expectRequest } = require("./../../test-helpers");
-const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
+const {
+  axiosMock, expectRequest
+} = require("./../../test-helpers.js");
+const api = require("./../../../src/client.js").createApiClient({
+  baseURL: "http://test.com"
+});
 
-describe('inventory/items', function() {
-  const token = 'I owe you a token';
-  
-  afterEach(function() {
+describe.oy("inventory/items", () => {
+  const token = "I owe you a token";
+  const jwtToken = "I owe you a JWT token";
+
+  afterEach(() => {
     axiosMock.restore();
-  })
-
-  it("should list items", function() {
-    axiosMock.onGet(`/items`).reply(expectRequest({ statusCode: 200, token }));
-    return api.inventory.items.all({ token });
   });
 
+  it("should list items", () => {
+    axiosMock.onGet("/items").reply(expectRequest({
+      statusCode: 200, token, jwtToken
+    }));
+    return api.inventory.items.all({
+      token,
+      jwtToken
+    });
+  });
+
+  it("should create a label", () => {
+    axiosMock.onPost("/items").reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.inventory.items.create({
+      jwtToken,
+      token,
+      label: {
+        name: "My label"
+      }
+    });
+  });
+
+  it("should create an item", () => {
+    axiosMock.onPost("/items").reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.inventory.items.create({
+      jwtToken,
+      token,
+      item: {
+        name: "My item"
+      }
+    });
+  });
+
+  it("should update a item", () => {
+    const itemId = "1234";
+    axiosMock.onPut(`/items/${itemId}`).reply(expectRequest({statusCode: 200, token, jwtToken}));
+    return api.inventory.items.update({
+      jwtToken,
+      token,
+      itemId,
+      item: {
+        name: "My Updated item it"
+      }
+    });
+  });
 });
