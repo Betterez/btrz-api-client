f232e9af11821005e415725d0e95f8d78f5361ae
Hernan Garcia <hernan@dynamicprogrammer.com>
2024-04-05 15:48:41 -0400

    generated types from src

--- types/initializedClient.d.ts
+++ types/initializedClient.d.ts
@@ -410,9 +410,30 @@ declare const _exports: {
             }) => any;
         };
         items: {
-            all: ({ token, query, headers }: {
+            all: ({ token, query, jwtToken, headers }: {
                 token: any;
                 query?: {};
+                jwtToken: any;
+                headers: any;
+            }) => any;
+            get: ({ itemId, token, jwtToken, query, headers }: {
+                itemId: any;
+                token: any;
+                jwtToken: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            create: ({ jwtToken, item, token, headers }: {
+                jwtToken: any;
+                item: any;
+                token: any;
+                headers: any;
+            }) => any;
+            update: ({ jwtToken, token, itemId, item, headers }: {
+                jwtToken: any;
+                token: any;
+                itemId: any;
+                item: any;
                 headers: any;
             }) => any;
         };
