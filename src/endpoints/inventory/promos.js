8f31a621c6cebcca33ee5ae878acb74e39254e35
Alejandro Cirino <alejandro@betterez.com>
2025-06-30 10:35:35 -0300

    [AC] #86dx090eu

--- src/endpoints/inventory/promos.js
+++ src/endpoints/inventory/promos.js
@@ -1,31 +1,30 @@
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {authorizationHeaders} = require("./../endpoints_helpers");
 
 function promosFactory({client, internalAuthTokenProvider}) {
-
-  function all({ token, query = {}, headers }) {
+  function all({token, query = {}, headers}) {
     return client.get("/promos", {
       params: query,
       headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
     });
   }
 
-  function get({ promoId, token, query = {}, headers }) {
+  function get({promoId, token, query = {}, headers}) {
     return client.get(`/promos/${promoId}`, {
       params: query,
       headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
     });
   }
 
-  function create({ jwtToken, promo, token, headers }) {
+  function create({jwtToken, promo, token, headers}) {
     return client({
       url: "/promos",
       method: "post",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
-      data: { promo }
+      data: {promo}
     });
   }
 
-  function remove({ jwtToken, promoId, token, headers }) {
+  function remove({jwtToken, promoId, token, headers}) {
     return client({
       url: `/promos/${promoId}`,
       method: "delete",
@@ -33,30 +32,39 @@ function promosFactory({client, internalAuthTokenProvider}) {
     });
   }
 
-  function update({ jwtToken, token, promoId, update, headers }) {
+  function update({jwtToken, token, promoId, update, headers}) {
     return client({
       url: `/promos/${promoId}`,
       method: "patch",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
-      data: { update }
+      data: {update}
     });
   }
 
-  function addRule({ jwtToken, token, promoId, rule, headers }) {
+  function patch({jwtToken, token, promoId, operations, headers}) {
+    return client({
+      url: `/promo/${promoId}`,
+      method: "patch",
+      headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
+      data: {operations}
+    });
+  }
+
+  function addRule({jwtToken, token, promoId, rule, headers}) {
     return client({
       url: `/promos/${promoId}/rules`,
       method: "post",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
-      data: { rule }
+      data: {rule}
     });
   }
 
-  function updateRule({ jwtToken, token, promoId, ruleId, rule, headers }) {
+  function updateRule({jwtToken, token, promoId, ruleId, rule, headers}) {
     return client({
       url: `/promos/${promoId}/rules/${ruleId}`,
       method: "put",
       headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers}),
-      data: { rule }
+      data: {rule}
     });
   }
 
@@ -65,11 +73,11 @@ function promosFactory({client, internalAuthTokenProvider}) {
     get,
     create,
     update,
+    patch,
     remove,
     addRule,
     updateRule
   };
-
 }
 
 module.exports = promosFactory;
