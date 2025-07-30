9e6635c2c7cdad2dbf14b1d84dfcc7f92ad0ac96
Guido Ambrosino <ambrosino.guido@gmail.com>
2025-07-30 16:54:44 -0300

    [GA] #86dxd0gwk add GET countries/{countryId} endpoint

--- src/endpoints/inventory/countries.js
+++ src/endpoints/inventory/countries.js
@@ -1,8 +1,7 @@
-const { authorizationHeaders } = require("./../endpoints_helpers");
+const {authorizationHeaders} = require("./../endpoints_helpers.js");
 
-function countriesFactory({ client, internalAuthTokenProvider }) {
-  
-  function all({ token, query = {}, headers }) {
+function countriesFactory({client, internalAuthTokenProvider}) {
+  function all({token, query = {}, headers}) {
     return client({
       url: "/countries",
       params: query,
@@ -10,10 +9,18 @@ function countriesFactory({ client, internalAuthTokenProvider }) {
     });
   }
 
-  return { 
-    all 
-  };
+  function get({token, id, query = {}, headers}) {
+    return client({
+      url: `/countries/${id}`,
+      params: query,
+      headers: authorizationHeaders({token, internalAuthTokenProvider, headers})
+    });
+  }
 
+  return {
+    all,
+    get
+  };
 }
 
 module.exports = countriesFactory;
--- test/endpoints/inventory/countries.test.js
+++ test/endpoints/inventory/countries.test.js
@@ -1,16 +1,29 @@
-const { axiosMock, expectRequest } = require("./../../test-helpers");
-const api = require("./../../../src/client").createApiClient({ baseURL: "http://test.com" });
+const {axiosMock, expectRequest} = require("./../../test-helpers.js");
+const api = require("./../../../src/client.js").createApiClient({baseURL: "http://test.com"});
 
-describe('inventory/countries', function() {
-  const token = 'I owe you a token';
-  
-  afterEach(function() {
+describe("inventory/countries", () => {
+  const token = "I owe you a token";
+
+  afterEach(() => {
     axiosMock.restore();
-  })
-
-  it("should list countries", function() {
-    axiosMock.onGet(`/countries`).reply(expectRequest({ statusCode: 200, token }));
-    return api.inventory.countries.all({ token });
   });
 
+  it("should list countries", () => {
+    axiosMock.onGet("/countries").reply(expectRequest({statusCode: 200, token}));
+    return api.inventory.countries.all({token});
+  });
+
+  it("should get a country by id", () => {
+    const countryId = "countryId";
+    axiosMock.onGet(`/countries/${countryId}`).reply(expectRequest({statusCode: 200, token}));
+    return api.inventory.countries.get({id: countryId, token});
+  });
+
+  it("should get a country by id and support query parameters", () => {
+    const countryId = "countryId";
+    const query = {param1: "value1"};
+
+    axiosMock.onGet(`/countries/${countryId}?param1=value1`).reply(expectRequest({statusCode: 200, token}));
+    return api.inventory.countries.get({id: countryId, query, token});
+  });
 });
