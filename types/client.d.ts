3c461e794e978ce8c4b20fc71992282e4f9b1f2d
Michel Romero <studentenherz@gmail.com>
2026-02-12 17:36:07 -0300

    generated types from src

--- types/client.d.ts
+++ types/client.d.ts
@@ -1133,6 +1133,43 @@ export function createApiClient(options: {
                 headers: any;
             }) => any;
         };
+        operationReasons: {
+            all: ({ token, jwtToken, query, headers }: {
+                token: any;
+                jwtToken: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            get: ({ token, jwtToken, id, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            update: ({ token, jwtToken, id, operationReason, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                operationReason: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            remove: ({ token, jwtToken, id, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            create: ({ token, jwtToken, operationReason, query, headers }: {
+                token: any;
+                jwtToken: any;
+                operationReason: any;
+                query?: {};
+                headers: any;
+            }) => any;
+        };
         parcelZones: {
             all: ({ token, query, headers }: {
                 token: any;
--- types/initializedClient.d.ts
+++ types/initializedClient.d.ts
@@ -1087,6 +1087,43 @@ declare const _exports: {
                 headers: any;
             }) => any;
         };
+        operationReasons: {
+            all: ({ token, jwtToken, query, headers }: {
+                token: any;
+                jwtToken: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            get: ({ token, jwtToken, id, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            update: ({ token, jwtToken, id, operationReason, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                operationReason: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            remove: ({ token, jwtToken, id, query, headers }: {
+                token: any;
+                jwtToken: any;
+                id: any;
+                query?: {};
+                headers: any;
+            }) => any;
+            create: ({ token, jwtToken, operationReason, query, headers }: {
+                token: any;
+                jwtToken: any;
+                operationReason: any;
+                query?: {};
+                headers: any;
+            }) => any;
+        };
         parcelZones: {
             all: ({ token, query, headers }: {
                 token: any;
