0d99d263562bce93b9300a83f61b9525a6883863
Michel Romero <studentenherz@gmail.com>
2026-03-19 10:16:49 -0300

    [MR] #86b8z44bq add accounts..saldoMax endpoints

--- src/endpoints/accounts/external-customers.js
+++ src/endpoints/accounts/external-customers.js
@@ -29,7 +29,36 @@ function externalCustomersFactory({client, internalAuthTokenProvider}) {
     });
   }
 
+  const saldoMax = {
+    /**
+     * GET /external-customers/ado - get SaldoMax user by email, phone or walletId.
+     * The querying precedence is (from higher to lower precedence):
+     *
+     * - email
+     * - phone
+     * - walletId
+     *
+     * Only the first option found is used in the lookup.
+     * @param {Object} opts
+     * @param {string} [opts.token] - API key
+     * @param {string} [opts.jwtToken] - JWT or internal auth symbol
+     * @param {{email?: string, phone?: string, walletId?: string}} opts.query - Query to get the SaldoMax user
+     * @param {Object} [opts.headers] - Optional headers
+     * @returns {Promise<import("axios").AxiosResponse<{ adoUsers: Array<Object> }>>}
+     * @throws When response is 4xx/5xx (400, 401, 404 EXTERNAL_WALLET_NOT_FOUND, 500)
+     */
+    get: ({token, jwtToken, query, headers}) => {
+      return client.get("/external-customers/ado", {
+        params: query,
+        headers: authorizationHeaders({token, jwtToken, internalAuthTokenProvider, headers})
+      });
+    },
+    create: registerSaldoMax
+  };
+
   return {
+    saldoMax,
+    // Keep here for backwards compat
     registerSaldoMax
   };
 }
