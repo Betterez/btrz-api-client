2c53c1de961dd851a0e7112ac4516838c61fd23a
Hernan Garcia <hernan@dynamicprogrammer.com>
2026-07-09 13:32:54 -0400

    [HG] CU-86bav6x0a

--- test/endpoints/accounts/ticket-movement-settings.test.js
+++ test/endpoints/accounts/ticket-movement-settings.test.js
@@ -17,7 +17,37 @@ describe("accounts/ticket-movement-settings", () => {
 
   it("should update ticket movement settings", () => {
     const ticketMovementSettings = {
-      enabled: true
+      preventMovementsAfterManifestHasBeenPrinted: true,
+      preventSeatmapOptimizationMovementsWithDifferentSeatClassOrFees: false,
+      preventMovementsWhenOriginalManifestIsBoarding: false,
+      originalManifestIsBoardingWhen: null,
+      preventMovementsWhenDestinationManifestIsBoarding: false,
+      destinationManifestIsBoardingWhen: null,
+      preventMovedTicketsFromBeingChanged: true,
+      preventMovedTicketsFromBeingCancelled: false,
+      preventMovementsWhenFareSpecificCapacityLimitsAreExceeded: true,
+      sendSMSNotifications: true,
+      allowSendingEmailNotification: true,
+      useConfirmationRecipientForNotifications: false,
+      maxMovementsPerTicket: 1,
+      maxMovementsPerTicketWithAuthorization: 2,
+      amenityGroupTransitions: {
+        allowAny: true,
+        allowedTransitions: null
+      },
+      timeRestrictions: {
+        originalTripRestrictions: {
+          cutoffMinutes: 0,
+          referencePoint: "original_passenger_departure_time"
+        },
+        newTripRestrictions: {
+          lowerBoundCutoffMinutes: 0,
+          lowerBoundReferencePoint: "original_passenger_departure_time",
+          upperBoundCutoffMinutes: 15,
+          upperBoundReferencePoint: "original_passenger_departure_time"
+        }
+      },
+      followingSeatBehaviorEnabled: true
     };
 
     axiosMock.onPut("/ticket-movement-settings")
